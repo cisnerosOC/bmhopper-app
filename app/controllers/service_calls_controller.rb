b194003b587eed65aa4e5598acd01fbf6ac8304c
Oscar Cisneros <cisneros>
2014-07-06 11:21:11 -0500

    Added views to Service_call controller

--- app/controllers/service_calls_controller.rb
+++ app/controllers/service_calls_controller.rb
@@ -4,8 +4,15 @@ class ServiceCallsController < ApplicationController
   end
 
   def create
-    @product = Product.create(params[:product])
-    flash[:success] = 'Product Successfully created.'
-    redirect_to "/products/#{@product.id}"
+    @service_call = ServiceCall.create(params[:service_call])
+    params[:service_call].each do |key,value|
+
+      @service_call.service_call_details.create({:name=>key,:value=>value})
+    end
+    flash[:success] = 'Service  Successfully created.'
+    redirect_to "/service_calls/new"
   end
 end
+
+
+  # Parameters: {"utf8"=>"✓", "authenticity_token"=>"eimwPZTtLP2HeEWtZamr5S7KAB0/fAw87cS+8mdPELw=", "service_call"=>{"service1"=>"true", "service2"=>"true", "service3"=>"false", "service4"=>"false", "service5"=>"false", "service6"=>"false"}, "commit"=>"Create Service call"}
--- app/models/service_call.rb
+++ app/models/service_call.rb
@@ -1,7 +1,10 @@
 class ServiceCall < ActiveRecord::Base
   has_many :service_call_details
   belongs_to :user
-  attr_accessor :vaccum_service
-
-  
+  attr_accessor :service1
+  attr_accessor :service2
+  attr_accessor :service3
+  attr_accessor :service4
+  attr_accessor :service5
+  attr_accessor :service6
 end
